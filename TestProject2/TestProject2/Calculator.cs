ce709bbaa771091e8a68889fdf3f7b601d1a7a4f
esp <espgrandia78@hotmail.com>
2025-02-16 18:42:06 +0800

    feat: practice unit test with rider

---
+++ TestProject2/TestProject2/Calculator.cs
@@ -0,0 +1,10 @@
+namespace TestProject2
+{
+    public class Calculator
+    {
+        public int Add(int first, int second)
+        {
+            return first + second;
+        }
+    }
+}
---
+++ TestProject2/TestProject2/Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestProject2
+{
+    [TestFixture]
+    public class Tests
+    {
+        private Calculator _calculator = new Calculator();
+
+        [Test]
+        public void first_1_second_2_add_should_return_3()
+        {
+            ResultShouldBe(3, 1, 2);
+        }
+
+        private void ResultShouldBe(int expected, int first, int second)
+        {
+            Assert.AreEqual(expected, _calculator.Add(first, second));
+        }
+    }
+}
